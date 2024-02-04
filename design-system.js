32549b62eaea846eea9d236d872d45b001f6875e
mrsaif888 <105549569+mrsaif888@users.noreply.github.com>
2024-02-04 00:28:37 -0800

    Set the js to comment

--- design-system.js
+++ design-system.js
@@ -1,91 +1,91 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('.header__menu a');
-    var previousLink = null;
+// document.addEventListener("DOMContentLoaded", function() {
+//     var links = document.querySelectorAll('.header__menu a');
+//     var previousLink = null;
   
-    // Add event listener to each link
-    links.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        // Prevent default link behavior
-        event.preventDefault();
+//     // Add event listener to each link
+//     links.forEach(function(link) {
+//       link.addEventListener('click', function(event) {
+//         // Prevent default link behavior
+//         event.preventDefault();
   
-        // Reset color of previously clicked link
-        if (previousLink !== null) {
-          previousLink.style.color = ''; // Reset to original color
-        }
+//         // Reset color of previously clicked link
+//         if (previousLink !== null) {
+//           previousLink.style.color = ''; // Reset to original color
+//         }
   
-        // Remove active class from all links
-        links.forEach(function(link) {
-          link.classList.remove('active');
-        });
+//         // Remove active class from all links
+//         links.forEach(function(link) {
+//           link.classList.remove('active');
+//         });
   
-        // Add active class to the clicked link
-        link.classList.add('active');
+//         // Add active class to the clicked link
+//         link.classList.add('active');
   
         
   
-        // Update the previous link to the current one
-        previousLink = link;
-      });
-    });
-  });
-  document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('.header__side-menu a');
-    var previousLink = null;
+//         // Update the previous link to the current one
+//         previousLink = link;
+//       });
+//     });
+//   });
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var links = document.querySelectorAll('.header__side-menu a');
+//     var previousLink = null;
   
-    // Add event listener to each link
-    links.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        // Prevent default link behavior
-        event.preventDefault();
+//     // Add event listener to each link
+//     links.forEach(function(link) {
+//       link.addEventListener('click', function(event) {
+//         // Prevent default link behavior
+//         event.preventDefault();
   
-        // Reset color of previously clicked link
-        if (previousLink !== null) {
-          previousLink.style.color = ''; // Reset to original color
-        }
+//         // Reset color of previously clicked link
+//         if (previousLink !== null) {
+//           previousLink.style.color = ''; // Reset to original color
+//         }
   
-        // Remove active class from all links
-        links.forEach(function(link) {
-          link.classList.remove('active');
-        });
+//         // Remove active class from all links
+//         links.forEach(function(link) {
+//           link.classList.remove('active');
+//         });
   
-        // Add active class to the clicked link
-        link.classList.add('active');
+//         // Add active class to the clicked link
+//         link.classList.add('active');
   
      
   
-        // Update the previous link to the current one
-        previousLink = link;
-      });
-    });
-  });
-  document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('.header__side-menuu a');
-    var previousLink = null;
+//         // Update the previous link to the current one
+//         previousLink = link;
+//       });
+//     });
+//   });
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var links = document.querySelectorAll('.header__side-menuu a');
+//     var previousLink = null;
   
-    // Add event listener to each link
-    links.forEach(function(link) {
-      link.addEventListener('click', function(event) {
-        // Prevent default link behavior
-        event.preventDefault();
+//     // Add event listener to each link
+//     links.forEach(function(link) {
+//       link.addEventListener('click', function(event) {
+//         // Prevent default link behavior
+//         event.preventDefault();
   
-        // Reset color of previously clicked link
-        if (previousLink !== null) {
-          previousLink.style.color = ''; // Reset to original color
-        }
+//         // Reset color of previously clicked link
+//         if (previousLink !== null) {
+//           previousLink.style.color = ''; // Reset to original color
+//         }
   
-        // Remove active class from all links
-        links.forEach(function(link) {
-          link.classList.remove('active');
-        });
+//         // Remove active class from all links
+//         links.forEach(function(link) {
+//           link.classList.remove('active');
+//         });
   
-        // Add active class to the clicked link
-        link.classList.add('active');
+//         // Add active class to the clicked link
+//         link.classList.add('active');
   
       
-        // Update the previous link to the current one
-        previousLink = link;
-      });
-    });
-  });
+//         // Update the previous link to the current one
+//         previousLink = link;
+//       });
+//     });
+//   });
   
   
